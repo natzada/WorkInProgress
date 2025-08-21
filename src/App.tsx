3eea8d219d4c7d0b59c232d99c98e3bd2897d500
JMarquesDev <joao.marquesdev2025@gmail.com>
2025-08-21 15:33:07 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+import SideBar from './components/SideBar'
+
+function App() {
+
+  return (
+    <>
+    <main className='flex h-screen w-screen bg-lightbg'>
+      <SideBar />
+    </main>  
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function SideBar() {
+    return (
+        <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-transparent text-black shadow'>
+            <i>A</i>
+            <i>B</i>
+            <i>C</i>
+            <i>D</i>
+            <i>E</i>
+        </div>
+    )
+}
+
+const SideBarIcon = ({ icon }: string) => {
+    return(
+        <div className='sidebar-icon'>
+            {icon}
+        </div>
+    )
+}
+
+export default SideBar
